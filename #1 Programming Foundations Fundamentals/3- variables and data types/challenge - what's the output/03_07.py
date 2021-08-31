f3d746b7356d706b6ccca522985f33682c6fe8ca
Mohamed Ali <dev.mohali22@gmail.com>
2021-09-01 00:02:59 +0200

    challenge - what's the output

---
+++ #1 Programming Foundations Fundamentals/3- variables and data types/challenge - what's the output/03_07.py
@@ -0,0 +1,40 @@
+
+
+# 1- this code print the string (challeng 1:)
+print("Challenge 1:")
+
+
+# 2- create two variables the same name (message & Message)
+# but python case sensitive, Here there is a difference between the variables
+
+# A message for the user
+message = "This is going to be tricky ;)"
+Message = "Very tricky!"
+
+
+# 3- this code print the variable => message,  contain a string ("This is going to be tricky ;")
+print(message)  # show the message on the screen
+
+
+# 4- here create a variable named => result, the value for this variable is two power three
+
+# Perform mathematical operations
+result = 2**3
+
+
+# 5- this code print a string contain => ("2**3 =") and value of result variable is 8
+print("2**3 =", result)
+
+
+# 6- here a new value has been set to the variable => result
+result = 5 - 3
+
+
+# 7- this code print a string contain => ("5 - 3 =") and a new value of result variable is 2
+# <<<<< BUT THIS CODE IS NOT WORKING BECAUSE OF COMMENT ON THE CODE >>>>>
+
+#print("5 - 3 =", result)
+
+
+# 8- this code print the string (Challenge complete!)
+print("Challenge complete!")
