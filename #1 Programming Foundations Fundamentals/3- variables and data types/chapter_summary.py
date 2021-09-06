b250cb44f4dd55b9af55052cfbfbd5fb07f39c92
Mohamed Ali <dev.mohali22@gmail.com>
2021-09-06 22:48:13 +0200

    Chapter Summary

---
+++ #1 Programming Foundations Fundamentals/3- variables and data types/chapter_summary.py
@@ -0,0 +1,95 @@
+##### Chapter Summary (variables and data types) #####
+#----------------------------------------------------#
+
+# What are the variables #
+# ---------------------- #
+# We use variables to save data in memory, to be used in our program
+# The variable is created by typing the variable name, and then marks an equal, and then typing the value for this variable
+
+
+# example
+name = "Mohamed ali"
+
+
+# What is allowed to write the variable name? #
+# ------------------------------------------- #
+
+# 1- Allowed by typing numbers to the variable name, but at the end of the variable name and not at the beginning of the variable name.
+
+# example
+name1 = "This is a name for variable"
+
+
+# 2- When you type a two-part variable name, you must separate them by typing this mark ( - )
+
+# example
+my_age = 34
+
+
+# Not allowed to write the variable name #
+# -------------------------------------- #
+
+# 1- The variable name is not allowed with an empty space between words
+
+# example
+my age = 34  # This is invalid
+
+
+# 2- Python's keywords are not allowed to names for variables, such as ==> ( if | else )
+
+# example
+if = 34  # This is invalid
+
+
+# 3- The numbers can not be used at the beginning of the variable name
+
+# example
+1name = "Mohamed ali"  # This is invalid
+
+
+# Noticeable #
+# ---------- #
+
+# Python is case sensitive
+# The variable name in large characters is not the same as small characters
+# ==> ( Age = 34 | age = 34 )
+# Python is dynamic language
+# we don't need to declare a variable's type before using it. Here, we declare the variable and use it at the same time.
+# The Python interpreter able to figure out the type
+# In some programming languages when you define the variable, you must specify the data type of this variable, these languages are called strict languages such as ( C | C# | C++ )
+
+
+# Numbers in Python #
+# ----------------- #
+
+# There are two types of numbers
+# Integer numbers such as ==> ( 1 | 5 | 20 | 70 )
+# Floating numbers such as ==> ( 1.5 | 5.10 | 20.6 | 70.9 )
+
+
+# Arithmetic operators #
+# -------------------- #
+
+# 1- ( + )
+# 2- ( - )
+# 3- ( * )
+# 4- ( / )
+# 5- ( % )
+# 6- ( ** )
+
+
+# Strings in Python #
+# ----------------- #
+
+# The strings are characters, numbers and spaces using the strings in writing a word or text sentence
+# The strings are written between the single quote mark or a double quote mark such as => ( "String here" | 'String here' )
+
+# example
+name = "Mohamed ali"
+
+# OR
+
+name = 'Mohamed ali'
+
+
+# The strings can not be written without single quote mark or a double quote mark
