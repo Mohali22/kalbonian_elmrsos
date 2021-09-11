3459174fb3bf344c602b01d0a5be92994c35a0a4
Mohamed Ali <dev.mohali22@gmail.com>
2021-09-11 12:45:07 +0200

    add iteration chapter

---
+++ #2 Programming Foundations Beyond the Fundamentals/2-iteration/chapter_summery.py
@@ -0,0 +1,26 @@
+##### Chapter Summary ( iteration ) #####
+# ------------------------------------- #
+
+# Introduction to iteration #
+# ------------------------- #
+
+# 1- what is iteration
+# In Python, the iterative statements are also known as looping statements or repetitive statements.
+# The iterative statements are used to execute a part of the program repeatedly as long as a given condition is True.
+
+# 2- To write code that iterates,
+# you commonly need a few specific pieces of information.
+# First, you need to specify the data that's being iterated.
+# Second, you need to describe what should happen to the data on each iteration
+# Finally, you need to indicate when the loop should stop
+# a bug that can occur when the ending condition is omitted or specified incorrectly
+# This creates a situation where the loop never stops
+# which may result in a program that freezes or stops responding, as well as heavy demand on the computer running the program,
+# which could slow it down or cause it to heat up.
+
+
+# Iterating through collections #
+# ----------------------------- #
+
+# Using for loops with lists or other collections allows us to work with sets of data without writing repetitive code
+# For loops are typically used when the number of iterations is known before entering the loop
