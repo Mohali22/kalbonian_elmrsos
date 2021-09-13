a4a8ab71094c131e5adc87ebf17a5d3ced94431d
Mohamed Ali <dev.mohali22@gmail.com>
2021-09-13 05:43:30 +0200

    Using External Codes

---
+++ #2 Programming Foundations Beyond the Fundamentals/3-external_code/chapter_summary.py
@@ -0,0 +1,24 @@
+##### Chapter Summary ( using external code ) #####
+# ----------------------------------------------- #
+
+
+# Comparing types of external code #
+# -------------------------------- #
+
+# what is a external code
+# It is a collection of code written by other programmers and shared with the public.
+# These codes are public codes that can be used in any project to shorten the time and not write these codes from scratch.
+# There are different types of external code files
+
+
+# 1- module
+# In Python, Modules are simply files containing Python code that can be imported inside another Python Program.
+# contains code, like variables or functions
+# Modules are included by keyword ==> (import)
+
+# 2- package or a library
+# library usually means collection of ready functions, classes, objects that you can use in your program.
+
+# 3- framework
+# Framework just gives you basic structure around which you will write your code to have the greater functionality of the system.
+# It will force you to work in a standard way, So indirectly It helps to make your application standardized.
