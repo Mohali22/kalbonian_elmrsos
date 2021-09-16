11b613937072041145af4fd5a6e8aa5c99f1f4be
Mohamed ali <dev.mohali22@gmail.com>
2021-09-16 17:33:00 +0200

    deba

---
+++ #2 Programming Foundations Beyond the Fundamentals/7-debugging/chapter_summary.py
@@ -0,0 +1,11 @@
+##### Chapter Summary ( debugging ) #####
+# ------------------------------------- #
+
+# Introduction to debugging #
+# ------------------------- #
+
+# what is debugging?
+# Debugging is the routine process of locating and removing computer program bugs, errors or abnormalities, 
+# which is methodically handled by software programmers via debugging tools. 
+# Debugging checks, detects and corrects errors (or "bugs") to allow proper program operation, according to set specifications
+
