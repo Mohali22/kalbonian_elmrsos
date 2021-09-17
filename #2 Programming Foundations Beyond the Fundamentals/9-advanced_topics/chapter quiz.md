e495a37fe400a69869654907d980bfd4d6a3f31d
Mohamed Ali <dev.mohali22@gmail.com>
2021-09-17 22:22:41 +0200

    finish #2 Programming Foundations Beyond the Fundamentals

---
+++ #2 Programming Foundations Beyond the Fundamentals/9-advanced_topics/chapter quiz.md
@@ -0,0 +1,24 @@
+# These are the answers to the chapter nine quiz
+
+## Question 1
+What is it called when a compiler keeps track of which items in memory are no longer needed and deletes them automatically?
+
+## Answer 1
+garbage collection
+
+
+## Question 2
+What's an advantage of using multithreading?
+
+## Answer 2
+It can make your computer programs faster and more responsive.
+
+
+
+## Question 3
+Which is the best description of what an algorithm is?
+
+
+## Answer 3
+a set of instructions that describes how to get the exact result you want
+
---
+++ #2 Programming Foundations Beyond the Fundamentals/9-advanced_topics/chapter_summary.py
@@ -0,0 +1,36 @@
+##### Chapter Summary ( advanced topics ) #####
+# ------------------------------------------- #
+
+
+# Memory management across languages #
+# ---------------------------------- #
+
+# A computer uses two main types of storage.
+# 1- hard disk (HD) There's the drive, which stores programs and data even when the computer is shut down.
+
+# 2- Random-access memory (RAM) is the memory that holds a copy of any code that's running,
+# as well as the results of any calculations and other manipulation that my programs do.
+
+
+# Garbage Collection #
+# ----------------------------- #
+
+# This is an automated process in which the compiler keeps track of which items in memory are no longer needed and deletes them.
+
+
+# Multithreading #
+# -------------- #
+
+# Multithreading specifically refers to the concurrent execution of more than one sequential set (thread) of instructions.
+# Multithreaded programming is programming multiple, concurrent execution threads.
+# These threads could run on a single processor.
+# Or there could be multiple threads running on multiple processor cores
+
+
+# algorithm #
+# -------------- #
+
+# A programming algorithm is a procedure or formula used for solving a problem.
+# It is based on conducting a sequence of specified actions in which these actions describe how to do something,
+# and your computer will do it exactly that way every time. An algorithm works by following a procedure,
+# made up of inputs. Once it has followed all the inputs, it will see a result, also known as output.
