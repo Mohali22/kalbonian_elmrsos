2376ebc17661b52c96454bf221de175a76a86b5c
Mohamed Ali <dev.mohali22@gmail.com>
2021-09-18 22:26:47 +0200

    finish oop fundamentals chapter

--- #3 Programming Foundations Object-Oriented Design/1-object_oriented_fundamentals/chapter_summary.py
+++ #3 Programming Foundations Object-Oriented Design/1-object_oriented_fundamentals/chapter_summary.py
@@ -37,9 +37,9 @@
 # ======= #
 
 # What is Object?
-# Object is an instance of a class. 
-# An object in OOPS is nothing but a self-contained component which consists of methods and properties to make a particular type of data useful. 
-# For example color name, table, bag, barking. 
+# Object is an instance of a class.
+# An object in OOPS is nothing but a self-contained component which consists of methods and properties to make a particular type of data useful.
+# For example color name, table, bag, barking.
 # When you send a message to an object, you are asking the object to invoke or execute one of its methods as defined in the class.
 
 # The object contains #
@@ -53,17 +53,16 @@
 # Behaviors are the tasks that an object performs
 # behaviors include the fact that a person can speak, run, walk, and eat
 #---------------------------------------------------------#
-# Literally anything can be modelled as an object in OOP as long as it has attributes and behaviours. 
+# Literally anything can be modelled as an object in OOP as long as it has attributes and behaviours.
 # This is because the purpose of OOP is to map real-world objects and their actions.
-
 
 
 # class #
 # ===== #
 
 # What is class?
-# Class is a blueprint or a set of instructions to build a specific type of object. 
-# It is a basic concept of Object-Oriented Programming which revolves around real-life entities. 
+# Class is a blueprint or a set of instructions to build a specific type of object.
+# It is a basic concept of Object-Oriented Programming which revolves around real-life entities.
 # Class determines how an object will behave and what the object will contain.
 # can use it to create as many objects based on that class.
 # Different classes let us create different types of objects
@@ -76,26 +75,49 @@
 # ==> ( methods are the verb of the class )
 
 # What is attributes?
-# Attributes are data stored inside a class or instance and represent the state or quality of the class or instance. 
+# Attributes are data stored inside a class or instance and represent the state or quality of the class or instance.
 # One can think of attributes as noun or adjective
 
 
 # There are four fundamental ideas in object-oriented programming to keep in mind when creating classes.
-# And they have the names 
-# 1- Abstraction. 
-# 2- Polymorphism. 
+# And they have the names
+# 1- Abstraction.
+# 2- Encapsulation.
 # 3- Inheritance.
-# 4- Encapsulation
-
+# 4- Polymorphism.
 
 
 # 1- Abstraction
-# Data abstraction is one of the most essential and important feature of object oriented programming. 
-# Abstraction means displaying only essential information and hiding the details. 
-# Data abstraction refers to providing only essential information about the data to the outside world, 
+# Data abstraction is one of the most essential and important feature of object oriented programming.
+# Abstraction means displaying only essential information and hiding the details.
+# Data abstraction refers to providing only essential information about the data to the outside world,
 # hiding the background details or implementation.
 
-# example of a man driving a car. 
-# The man only knows that pressing the accelerators will increase the speed of car or applying brakes will stop the car but he does not know about how on pressing accelerator the speed is actually increasing, 
-# he does not know about the inner mechanism of the car or the implementation of accelerator, brakes etc in the car. 
+# example of a man driving a car.
+# The man only knows that pressing the accelerators will increase the speed of car or applying brakes will stop the car but he does not know about how on pressing accelerator the speed is actually increasing,
+# he does not know about the inner mechanism of the car or the implementation of accelerator, brakes etc in the car.
 # This is what abstraction is.
+
+
+# 2- Encapsulation
+# The process of wrapping up variables and methods into a single entity is known as Encapsulation.
+# It is one of the underlying concepts in object-oriented programming (OOP).
+# It acts as a protective shield that puts restrictions on accessing variables and methods directly,
+# and can prevent accidental or unauthorized modification of data.
+# Encapsulation also makes objects into more self-sufficient, independently functioning pieces.
+
+# How is the encapsulated data accessed?
+# The public setXXX() and getXXX() methods are the access points of the instance variables of the class.
+# Normally, these methods are referred to as getters and setters.
+# Therefore, any class that wants to access the variables should access them through these getters and setters.
+
+
+# 3- Inheritance
+# Inheritance in OOP is the process of inheriting properties and methods from another class without having to repeat the same properties and methods in a new class,
+# in addition to taking advantage of code reuse.
+# The class to be inherited from is called the superclass or parental class.
+# One of the advantages of inheritance is that if you make a change in a superclass,
+# It will automatically filter and affect all subclasses.
+
+
+# 4- Polymorphism
