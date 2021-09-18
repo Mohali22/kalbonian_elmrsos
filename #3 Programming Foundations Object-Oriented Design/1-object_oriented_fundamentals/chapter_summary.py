32fb25582cecd66a5b98f554175bfc1fd69a90d0
Mohamed Ali <dev.mohali22@gmail.com>
2021-09-18 05:32:07 +0200

    start #3 Programming Foundations Object-Oriented Design course

---
+++ #3 Programming Foundations Object-Oriented Design/1-object_oriented_fundamentals/chapter_summary.py
@@ -0,0 +1,39 @@
+##### Chapter Summary ( Object-oriented fundamentals ) #####
+# -------------------------------------------------------- #
+
+# Object-oriented thinking #
+# ------------------------ #
+
+# Differences between Procedural and OOP #
+# ========================================
+
+# 1- What is Procedural Programming?
+# Procedural Programming may be the first programming paradigm that a new developer will learn.
+# Fundamentally, the procedural code is the one that directly instructs a device on how to finish a task in logical steps.
+# This paradigm uses a linear top-down approach and treats data and procedures as two different entities.
+# Based on the concept of a procedure call, Procedural Programming divides the program into procedures,
+# which are also known as routines or functions, simply containing a series of steps to be carried out.
+# Simply put, Procedural Programming involves writing down a list of instructions to tell the computer what it should do step-by-step to finish the task at hand.
+
+# What is Object Oriented Programming?
+# Object oriented programming can be defined as a programming model which is based upon the concept of objects.
+# Objects contain data in the form of attributes and code in the form of methods.
+# In object oriented programming, computer programs are designed using the concept of objects that interact with real world.
+# Object oriented programming languages are various but the most popular ones are class-based,
+# meaning that objects are instances of classes, which also determine their types.
+
+
+# What are the advantages of object-oriented programming #
+# ====================================================== #
+
+# 1- Re-usability
+# 2- Data Redundancy
+# 3- Code Maintenance
+# 4- Better productivity
+# 5- Easy troubleshooting
+
+
+# Objects #
+# ======= #
+
+#
