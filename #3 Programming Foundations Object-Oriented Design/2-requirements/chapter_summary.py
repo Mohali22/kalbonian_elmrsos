680c0efb25d354ac6fc68f7d9066d58a2b6fc7ec
Mohamed ali <dev.mohali22@gmail.com>
2021-09-19 15:21:09 +0200

    add a requirements chapter

---
+++ #3 Programming Foundations Object-Oriented Design/2-requirements/chapter_summary.py
@@ -0,0 +1,14 @@
+##### Chapter Summary ( requirements ) #####
+# ---------------------------------------- #
+
+
+# Defining requirements #
+# --------------------- #
+
+# It is knowing the main requirements from the client and a comprehensive description of what he wants the application to provide
+# 1- What does the application provide?
+# 2- What problem does the app solve?
+# 3- The purpose of creating the application
+
+# You should focus only on the basic requirements and not on how to implement them.
+# The advantages provided by the application are determined
